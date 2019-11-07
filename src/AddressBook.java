4d54e51e307b5d6bc582ef944b9d4247444e85a5
Mariam05 <tomariam@gmail.com>
2019-11-07 11:45:45 -0500

    Added a view, IO functionality

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,40 +1,71 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
-public class AddressBook {
+import javax.swing.DefaultListModel;
 
-	private List<BuddyInfo> buddyInfos; 
-	
+public class AddressBook extends DefaultListModel<BuddyInfo> {
+
 	public AddressBook() {
-		buddyInfos = new ArrayList<BuddyInfo>();
+		super();
 	}
 	
+	public int size() {
+		return super.getSize();
+	}
+
+	public void clear() {
+		super.clear();
+	}
+
 	public void addBuddy(BuddyInfo buddy) {
 		if (buddy != null) {
-			buddyInfos.add(buddy);
+			super.addElement(buddy);
 		}
-		
 	}
-	
+
 	public BuddyInfo removeBuddy(int index) {
-		if (index >=0 && index < buddyInfos.size()) {
-			return this.buddyInfos.remove(index);
+		if (index >= 0 && index < super.getSize()) {
+			return super.remove(index);
 		}
 		return null;
 	}
 	
-	private void emptyMethod() {
-		return;
+	public void save(String filename) {
+		try {
+			FileWriter writer = new FileWriter(filename);
+			for(Object b : this.toArray()) {
+				BuddyInfo bd = (BuddyInfo)b;
+				writer.write(bd.toString() + "\n");
+			}
+			
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
+
 	
+	public void importAddressBook() {
+		//reads the exported file and recreats the addressBook. Use BufferedReader to import. 
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Address book");
 		BuddyInfo bud = new BuddyInfo("Homer", "Road", "123");
+		BuddyInfo bud2 = new BuddyInfo("Mr.Buddy", "111 Fake St.", "6131234567");
+		BuddyInfo bud3 = BuddyInfo.importBuddy("Mrs.Buddy,111 Real St.,613wrongnum,38");
 		AddressBook addresses = new AddressBook();
 		addresses.addBuddy(bud);
-		addresses.removeBuddy(0);
-
+		addresses.addBuddy(bud2);
+		addresses.addBuddy(bud3);
+		addresses.save("Buddies.txt");
+		
 	}
 
 }
--- src/BuddyInfo.java
+++ src/BuddyInfo.java
@@ -1,13 +1,39 @@
+import java.util.Scanner;
+
 public class BuddyInfo {
 
 	private String name;
 	private String address;
 	private String phoneNum;
+	private int age;
 	
 	public BuddyInfo(String name, String address, String phoneNum) {
 		this.name = name;
 		this.address = address;
 		this.phoneNum = phoneNum;
+		this.age = age;
+	}
+	
+	public BuddyInfo(BuddyInfo aBuddy) {
+		this.name = aBuddy.getName();
+		this.address = aBuddy.getAddress();
+		this.phoneNum = aBuddy.getPhoneNum();
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public boolean isOver18() {
+		return age > 18;
+	}
+	
+	public String greetBuddy() {
+		return "Hey there bud!";
 	}
 	
 	public String getName() {
@@ -26,7 +52,7 @@ public class BuddyInfo {
 		this.address = address;
 	}
 
-	public String getphoneNum() {
+	public String getPhoneNum() {
 		return this.phoneNum;
 	}
 
@@ -34,10 +60,31 @@ public class BuddyInfo {
 		this.phoneNum = phoneNum;
 	}
 	
-	public static void main(String[] args) {
-		BuddyInfo bud = new BuddyInfo("Homer", "Road", "123");
-		AddressBook addresses = new AddressBook();
-
+	public boolean isEquals(BuddyInfo bI) {
+		return bI.address.equals(this.address) && bI.name.equals(this.name) && bI.phoneNum.equals(this.phoneNum);
 	}
+	
+	public String toString() {
+		if(age == 0) return name + "||" + address + "||" + phoneNum ;
+		return name + "||" + address + "||" + phoneNum + "||" + age;
+	}
+	
+	public static BuddyInfo importBuddy(String input) {
+		
+		Scanner dscanner = new Scanner(input).useDelimiter("\\s*,\\s*");
+		String name = dscanner.next();
+		String address = dscanner.next();
+		String phoneNum = dscanner.next();
+		int age = dscanner.nextInt();
+		dscanner.close();
+		
+		BuddyInfo newBud = new BuddyInfo(name, address, phoneNum);
+		newBud.setAge(age);
+		
+		
+		return newBud;
+		
+	}
+	
 
 }
